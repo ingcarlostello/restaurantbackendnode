b9be1d00cb9a0dd2d436e0034751adfc474d304d
Carlos Tello <ingcarlostello@hotmail.com>
2022-08-07 22:11:53 -0500

    creacion de backend restaurante con mongo db

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '89a6f47af35df44711911e0db60f709f'),
+    },
+  },
+});
